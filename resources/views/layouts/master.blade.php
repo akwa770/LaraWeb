d8e914c831e8db4dceb90c1b6e86849832df3ffb
מנחם אקוע <akwa770@gmail.com>
2019-01-06 23:26:05 +0200

    add custom color to nav

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -86,28 +86,14 @@ scratch. This page gets rid of all links and provides the needed markup only.
 
 
          
-          <li class="nav-item has-treeview menu-open">
-            <a href="#" class="nav-link active">
+          <li class="nav-item has-treeview">
+            <a href="#" class="nav-link">
               <i class="nav-icon fas fa-tachometer-alt"></i>
               <p>
                 Management
                 <i class="right fa fa-angle-left"></i>
               </p>
             </a>
-            <ul class="nav nav-treeview">
-              <li class="nav-item">
-                <a href="#" class="nav-link active">
-                  <i class="fa fa-circle-o nav-icon"></i>
-                  <p>Active Page</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="#" class="nav-link">
-                  <i class="fa fa-circle-o nav-icon"></i>
-                  <p>Inactive Page</p>
-                </a>
-              </li>
-            </ul>
           </li>
           <li class="nav-item">
             <router-link to="/profile" class="nav-link">
