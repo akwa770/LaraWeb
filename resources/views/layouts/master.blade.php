73123cfcc549740ad9e80c77f6291bba6f77e3ac
מנחם אקוע <akwa770@gmail.com>
2019-01-14 12:51:20 +0200

    redirect after register & fix profile image bug

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -59,16 +59,17 @@ scratch. This page gets rid of all links and provides the needed markup only.
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          @if (Auth::user()->photo !== 'user.png')
+          @if (Auth::user()->photo !== 'profile.png')
           
           <img src="./images/profile/{{ Auth::user()->photo }}" class="img-circle elevation-2" alt="User Image">
           @else
-          <img src="./images/user.png" class="img-circle elevation-2" alt="User Image">
+          <img src="./images/profile.png" class="img-circle elevation-2" alt="User Image">
               
           @endif
         </div>
         <div class="info">
-          <a href="#" class="d-block">
+          <a href="{{ url('home#/profile') }}
+            " class="d-block">
             {{ Auth::user()->name }}
             <p>{{ Auth::user()->type }}</p>
           </a>
